c2dde1df5574317f754d7845c04aea2a10a5a9ec
dongchan-K <ehdcks1028@gmail.com>
2021-07-19 23:14:52 +0900

    feat: mongoDB를 활용한 todolist 모델 구현

---
+++ todo-back/src/models/todos.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const TodosSchema = new Schema({
+  id: mongoose.Types.ObjectId,
+  content: String,
+  isDone: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Todos = mongoose.model('Todos', TodosSchema);
+
+export default Todos;
