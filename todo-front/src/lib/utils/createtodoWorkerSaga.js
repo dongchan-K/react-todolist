b16069f19f6f273c43e9146a4e542324cf84f9eb
dongchan-K <ehdcks1028@gmail.com>
2021-07-21 16:01:31 +0900

    fix: 불필요한 코드 삭제

--- todo-front/src/lib/utils/createtodoWorkerSaga.js
+++ todo-front/src/lib/utils/createtodoWorkerSaga.js
@@ -7,13 +7,12 @@ export default function todoWorkerSaga(type, request) {
   return function* (action) {
     try {
       const res = yield call(request, action.payload);
-      console.log(res);
+
       yield put({
         type: SUCCESS,
         payload: res.data,
       });
     } catch (e) {
-      console.log(e);
       yield put({
         type: FAILURE,
         payload: e,
